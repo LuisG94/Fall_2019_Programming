97252303afdb91aea549fe91f0956a5ec09d368e
LuisG94 <lagboi94@yahoo.com>
2019-09-30 00:03:50 -0500

    Update Source.cpp

--- Stack_Reverse/Stack_Reverse/Source.cpp
+++ Stack_Reverse/Stack_Reverse/Source.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#pragma warning (disable:4996)
 
 struct Queue
 {
 	int offset;
 	int ndata;
-	char* data;
+	int* data;
 	int array_size;
 };
 
@@ -14,17 +15,17 @@ void init(Queue *s, int array_size)
 	s->array_size = array_size;
 	s->ndata = 0;
 	s->offset = 0;
-	s->data = (char*)malloc(sizeof(char) * array_size);
+	s->data = (int*)malloc(sizeof(int) * array_size);
 }
 
-void queue(Queue *s, char value)
+void enqueue(Queue *s, int value)
 {
 	int index = (s->offset + s->ndata) % s->array_size;
 	s->data[index] = value;
 	s->ndata++;
 }
 
-char dequeue(Queue *s)
+int dequeue(Queue *s)
 {
 	int index = s->offset++;
 	s->offset %= s->array_size;
@@ -32,11 +33,32 @@ char dequeue(Queue *s)
 	return s->data[index];
 }
 
+void print(Queue *s)
+{
+	for (int i = 0; i < s->ndata; ++i)
+	{
+		printf("%d ", s->data[(s->offset + i) & s->array_size]);
+	}
+}
+
 
 int main()
 {
-	
+	int array_size = 6;
 
+	Queue q;
+	init(&q, array_size);
+
+	for (int i = 0; i < array_size; ++i)
+	{
+		int value = 0;
+		printf("Enter a value: ");
+		int r = scanf("%d\n", &value);
+		enqueue(&q, r);
+	}
+
+
+	print(&q);
 
 
 	system("pause");
